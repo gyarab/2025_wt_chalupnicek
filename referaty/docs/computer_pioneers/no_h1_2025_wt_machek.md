6592cc5eb365fd14606a2b3b9d2dc3cafa9fe758
Václav Chalupníček <vaclav.chalupnicek@gyarab.cz>
2025-10-10 17:15:08 +0200

    updated repo structure

--- referaty/docs/computer_pioneers/no_h1_2025_wt_machek.md
+++ referaty/docs/computer_pioneers/no_h1_2025_wt_machek.md
@@ -1,3 +1,3 @@
 from <https://github.com/gyarab/2025_wt_machek>
 
-Repository not found: 2025_wt_machek
+
--- referaty/download_pages.py
+++ referaty/download_pages.py
@@ -1,8 +1,8 @@
 #!./venv/bin/python3
 
-# For each item in repo_list.txt download referat.md file as ./referaty/docs/[referat title].md
+# For each item in repo_list.txt get referat.md file as ./referaty/docs/[referat title].md
 
-import httpx
+# import httpx
 import re
 import sys
 
@@ -10,33 +10,33 @@ sys.stdout.reconfigure(encoding="utf-8")
 
 
 with open("repo_list.txt") as file:
-    for repo_name in file.readlines():
-        repo_name = repo_name.strip()
-        if repo_name.startswith('#'):
+    for repo_path in file.readlines():
+        repo_path = repo_path.strip()
+        if repo_path.startswith('#'):
             continue
-        if not repo_name:
+        if not repo_path:
             continue
-        print(f"{repo_name}")
+        print(f"{repo_path}")
 
 
-        # get_file(repo_name, base_url + "README.md", "computer_pioneers")
-        # get_file(repo_name, base_url + "color.md", "brands")
-        # def get_file(repo_name, url, target_dir):
+        # get_file(repo_path, base_url + "README.md", "computer_pioneers")
+        # get_file(repo_path, base_url + "color.md", "brands")
+        # def get_file(repo_path, url, target_dir):
 
-
-        # base_url = f"https://raw.githubusercontent.com/gyarab/{repo_name}/refs/heads/main/"
+       
+        # base_url = f"https://raw.githubusercontent.com/gyarab/{repo_path}/refs/heads/main/"
         # url = base_url + "README.md"
         # response = httpx.get(url)
         # text = response.text
 
-        base_path = f"../2025_wt/{repo_name}/README.md"
+        base_path = f"{repo_path}/README.md"
         try:
             with open(base_path, "r", encoding='utf-8') as in_file:
                 text = in_file.read()
         except FileNotFoundError:
-            text = f"Repository not found: {repo_name}"
+            text = f"Repository not found: {repo_path}"
 
-        subject = f"no_h1_{repo_name}"
+        subject = f"no_h1_{repo_path}"
         # try to find subject in markdown headline
         text_lines = text.split('\n')
         for line in text_lines:
@@ -45,13 +45,13 @@ with open("repo_list.txt") as file:
                 break
 
         # prepend link to original repo
-        text = f"from <https://github.com/gyarab/{repo_name}>\n\n{text}"
+        text = f"from <https://github.com/gyarab/{repo_path}>\n\n{text}"
 
         out_file_name = subject.replace(' ', '_')
         out_file_name = re.sub('[^a-zA-Z0-9_-]+', '', out_file_name)
         out_file_name += ".md"
 
-        print(f"                        {out_file_name}     {subject}")
+        print(f" - {out_file_name}         {subject}")
 
         target_dir = "computer_pioneers"
         with open(f"referaty/docs/{target_dir}/{out_file_name}", "w", encoding='utf-8') as out_file:
